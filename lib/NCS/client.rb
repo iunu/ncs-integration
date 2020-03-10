a0aa8b64a7438fea7cbe55b818810881bfb7ce4f
Joseph B. Ottinger <joeo@enigmastation.com>
2020-03-10 11:24:30 -0400

    Splitting data model out for sanity

--- lib/NCS/client.rb
+++ lib/NCS/client.rb
@@ -1,10 +1,11 @@
-# frozen_string_literal: true
+require_relative 'data_model.rb' # frozen_string_literal: true
 require 'net/http'
 require 'uri'
 require 'httparty'
 
 # NCS module
 module NCS
+  include DataModel
   include Constants
   include Errors
 
@@ -408,82 +409,4 @@ module NCS
     end
   end
 
-  ItemModel = JSON.parse(<<~ITEM_MODEL
-    {
-           "Id": 2,
-           "ProductCategoryName": "Buds",
-           "Name": "Buds Item",
-           "UnitOfMeasureName": "Ounces",
-           "AdministrationMethod": null,
-           "StrainName": "Spring Hill Kush",
-           "IntermediaryProductUsed": false,
-           "UnitCbdPercent": null,
-           "UnitCbdContent": null,
-           "UnitCbdContentUnitOfMeasureName": null,
-           "UnitThcPercent": null,
-           "UnitThcContent": null,
-           "UnitThcContentUnitOfMeasureName": null,
-           "UnitVolume": null,
-           "UnitVolumeUnitOfMeasureName": null,
-           "UnitWeight": null,
-           "UnitWeightUnitOfMeasureName": null,
-           "ServingSize": null,
-           "SupplyDurationDays": null,
-           "Ingredients": null,
-           "Intermediaries": [
-                {
-                    "Id": 1,
-                    "Name": "Interm 1",
-                    "Type": "Mush"
-                },
-                {
-                    "Name": "Interm 2",
-                    "Type": "Tar"
-                }
-            ]
-        }
-  ITEM_MODEL
-  )
-  ItemCategoryModel = JSON.parse(<<~ITEM_CATEGORY_MODEL
-    {
-      "Name": "Infused",
-      "ProductCategoryType": "InfusedEdible",
-      "QuantityType": "CountBased",
-      "DefaultLabTestingState": "NotSubmitted",
-      "RequiresApproval": false,
-      "RequiresAdministrationMethod": false,
-      "RequiresStrain": true,
-      "RequiresUnitCbdPercent": false,
-      "RequiresUnitCbdContent": false,
-      "RequiresUnitThcPercent": false,
-      "RequiresUnitThcContent": false,
-      "RequiresUnitVolume": false,
-      "RequiresUnitWeight": false,
-      "RequiresServingSize": false,
-      "RequiresSupplyDurationDays": false,
-      "UnitQuantityMultiplier": null,
-      "UnitQuantityUnitOfMeasureName": null,
-      "RequiresIngredients": false,
-      "RequiresProductPhoto": false,
-      "CanContainSeeds": true,
-      "CanBeRemediated": true
-    }
-  ITEM_CATEGORY_MODEL
-  )
-  RoomModel = JSON.parse(<<~ROOM_MODEL
-    {
-        "id": 0,
-        "locationId": 0,
-        "organizationId": 0,
-        "vendorId": 0,
-        "name": "string",
-        "createDate": "2020-03-09T18:15:13.028Z",
-        "updateDate": "2020-03-09T18:15:13.028Z"
-    }
-  ROOM_MODEL
-  )
-
-  def extract_keys(model)
-    model.keys
-  end
 end
