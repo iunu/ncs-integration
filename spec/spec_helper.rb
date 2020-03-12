9a9bf7d3111954efa5e68bab87c9ddbe2a95d998
Carlos Betancourt Carrero <carbetacar@gmail.com>
2020-03-12 18:29:34 -0300

    Spec helper rubocop fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,12 @@
 require 'yaml'
 
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-$spec_credentials = YAML.load_file('./spec/test_credentials.yml')
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+$spec_credentials = YAML.load_file('./spec/test_credentials.yml') # rubocop:disable Style/GlobalVars
 
 require 'webmock/rspec'
 require 'NCS'
 
-Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
+Dir[File.expand_path(File.join(File.dirname(__FILE__), 'support', '**', '*.rb'))].each {|f| require f } # rubocop:disable Lint/NonDeterministicRequireOrder
 
 RSpec.configure do |config|
   config.include ConfigurationHelper
--- spec/support/configuration_helper.rb
+++ spec/support/configuration_helper.rb
@@ -1,10 +1,9 @@
 module ConfigurationHelper
   def configure_client
     NCS.configure do |config|
-      config.api_key = $spec_credentials['api_key']
-      config.uri = $spec_credentials['uri']
-      config.debug = false
-      config.debug = $spec_credentials['debug'] if $spec_credentials['debug']
+      config.api_key = $spec_credentials.fetch('api_key') # rubocop:disable Style/GlobalVars
+      config.uri = $spec_credentials.fetch('uri') # rubocop:disable Style/GlobalVars
+      config.debug = $spec_credentials.fetch('debug') if $spec_credentials.fetch('debug') # rubocop:disable Style/GlobalVars
     end
   end
 end
