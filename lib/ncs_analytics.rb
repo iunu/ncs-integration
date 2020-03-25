ed65c13f10371419e43af38ac5fcb83d0e23888e
Carlos Betancourt Carrero <carbetacar@gmail.com>
2020-03-24 22:12:39 -0300

    Don't include the client file yet

--- lib/ncs_analytics.rb
+++ lib/ncs_analytics.rb
@@ -2,7 +2,6 @@ require 'ncs_analytics/version'
 require 'ncs_analytics/configuration'
 require 'ncs_analytics/errors'
 require 'ncs_analytics/base'
-require 'ncs_analytics/client'
 
 module NcsAnalytics
   def self.configuration
