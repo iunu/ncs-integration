ccef63b43a452ca10262fe8f83a549b7c77e0c38
Carlos Betancourt Carrero <carbetacar@gmail.com>
2020-03-24 22:10:09 -0300

    Use ENV variables for testing credentials

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,15 @@
 require 'yaml'
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
-$spec_credentials = YAML.load_file('./spec/test_credentials.yml') # rubocop:disable Style/GlobalVars
+
+if File.file?('./spec/test_credentials.yml') # rubocop:disable Style/ConditionalAssignment
+  $spec_credentials = YAML.load_file('./spec/test_credentials.yml') # rubocop:disable Style/GlobalVars
+else
+  $spec_credentials = { # rubocop:disable Style/GlobalVars
+    'api_key': ENV['NCS_TEST_API_KEY'],
+    'uri': ENV['NCS_TEST_API_URI']
+  }.freeze
+end
 
 require 'webmock/rspec'
 require 'ncs_analytics'
