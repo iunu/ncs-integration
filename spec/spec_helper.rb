6be0488ac8fe3f57d264756c2867abe310db95e5
Carlos Betancourt Carrero <carbetacar@gmail.com>
2020-03-24 23:00:41 -0300

    Keep credentials on a single place

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,4 @@
-require 'yaml'
-
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
-
-if File.file?('./spec/test_credentials.yml') # rubocop:disable Style/ConditionalAssignment
-  $spec_credentials = YAML.load_file('./spec/test_credentials.yml') # rubocop:disable Style/GlobalVars
-else
-  $spec_credentials = { # rubocop:disable Style/GlobalVars
-    'api_key': ENV['NCS_TEST_API_KEY'],
-    'uri': ENV['NCS_TEST_API_URI']
-  }.freeze
-end
 
 require 'webmock/rspec'
 require 'ncs_analytics'
--- spec/support/configuration_helper.rb
+++ spec/support/configuration_helper.rb
@@ -1,9 +1,18 @@
+require 'yaml'
+
+spec_credentials = { # rubocop:disable Lint/UselessAssignment
+  api_key: ENV['NCS_TEST_API_KEY'],
+  uri: ENV['NCS_TEST_API_URI']
+}.freeze
+
+spec_credentials = YAML.load_file('./spec/test_credentials.yml').transform_keys(&:to_sym) if File.file?('./spec/test_credentials.yml') # rubocop:disable Layout/LineLength, Lint/UselessAssignment
+
 module ConfigurationHelper
   def configure_client
     NcsAnalytics.configure do |config|
-      config.api_key = $spec_credentials.fetch('api_key') # rubocop:disable Style/GlobalVars
-      config.uri = $spec_credentials.fetch('uri') # rubocop:disable Style/GlobalVars
-      config.debug = $spec_credentials.has_key?('debug') # rubocop:disable Style/GlobalVars
+      config.api_key = spec_credentials.fetch(:api_key)
+      config.uri = spec_credentials.fetch(:uri)
+      config.debug = spec_credentials.has_key?(:debug)
     end
   end
 end
